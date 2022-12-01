aa77daac6f0ae4e9b4bc670770553bbd4c7785cd
Christian Garcia <christiangarcia.cg77@gmail.com>
2022-11-30 17:49:40 -0800

    worked on encryptRSA and added key & iv generation

--- Sender.java
+++ Sender.java
@@ -1,37 +1,61 @@
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 
 public class Sender {
   public static void main(String[] args) {
     //String myPrivateKey = "";
-    String p2PublicKey = "";
+    Key p2PublicKey;
     String message = "";
     String AESKey = "";
     String ciphertext = "";
     String encryptedAESKey = "";
     String MAC = "";
+    IvParameterSpec iv;
 
-    //Creating a KeyGenerator object
-    KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-
-    //Creating a SecureRandom object
-    SecureRandom secRandom = new SecureRandom();
-
-    //Initializing the KeyGenerator
-    keyGen.init(secRandom);
-
-    //Creating/Generating a key
-    Key key = keyGen.generateKey();
-    
     try {
+      //Creating KeyPair generator object
+      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+      
+      //Initializing the key pair generator
+      keyPairGen.initialize(2048);
+      
+      //Generating the pair of keys
+      KeyPair pair = keyPairGen.generateKeyPair();
+
+      iv = generateIv();
+
+
+
+      //Creating a KeyGenerator object
+      KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+
+      //Creating a SecureRandom object
+      SecureRandom secRandom = new SecureRandom();
+
+      //Initializing the KeyGenerator
+      keyGen.init(secRandom);
+
+      //Creating/Generating a key
+      Key key = keyGen.generateKey();
+    
+    
       ciphertext = encryptAES(message, AESKey);
-      encryptedAESKey = encryptRSA(AESKey, p2PublicKey);
-      MAC = generateMAC(encryptedAESKey+ciphertext);
+      encryptedAESKey = encryptRSA(AESKey, p2PublicKey, iv);
+      MAC = generateMAC(encryptedAESKey+ciphertext, key);
       // write MAC+encryptedAESKey+ciphertext to TransmittedData
     } catch (Exception e) {
       // handle exception
@@ -40,14 +64,43 @@ public class Sender {
     
   }
 
+  public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+    keyGenerator.init(n);
+    SecretKey key = keyGenerator.generateKey();
+    return key;
+  }
+
+  public static IvParameterSpec generateIv() {
+    byte[] iv = new byte[16];
+    new SecureRandom().nextBytes(iv);
+    return new IvParameterSpec(iv);
+  }
+
   private static String encryptAES(String message, String key) {
     String result = "";
     return result;
   }
 
-  private static String encryptRSA(String message, String key) {
+  private static String encryptRSA(String message, Key key, IvParameterSpec iv) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
     String result = "";
-    return result;
+      
+          
+
+    //Creating a Cipher object
+    Cipher cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding");
+      
+    //Initializing a Cipher object
+    cipher.init(Cipher.ENCRYPT_MODE, key);
+  
+    //Adding data to the cipher
+    byte[] input = message.getBytes();	  
+    cipher.update(input);
+  
+    //encrypting the data
+    byte[] cipherText = cipher.doFinal();	 
+    return new String(cipherText, "UTF8");
+    
   }
 
   private static String generateMAC(String message, Key key) throws NoSuchAlgorithmException, InvalidKeyException {	 
