c7cf3065ded2b1953c9896cdbfae1d979fe8f2cf
Christian Garcia <christiangarcia.cg77@gmail.com>
2022-12-07 13:11:34 -0800

    receiver can now read transmitted data

--- Receiver.java
+++ Receiver.java
@@ -4,11 +4,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 import java.io.Reader;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Scanner;
 
@@ -22,12 +29,41 @@ import javax.crypto.spec.IvParameterSpec;
 public class Receiver {
   public static void main(String[] args) {
     try {
-      //String data = readData("TransmittedData.txt");
+      Path filePath = Paths.get("TransmittedData.txt");
+      long fileSize = Files.size(filePath);
+      
+      byte[] data = read("TransmittedData.txt");
+      byte[] MAC = Arrays.copyOfRange(data, 0, 32);
+      byte[] encryptedAESKey = Arrays.copyOfRange(data, 32, 288);
+      byte[] cipherText = Arrays.copyOfRange(data, 288, (int)(fileSize - 288));
+
+      System.out.println(Base64.getEncoder().encodeToString(MAC));
+      System.out.println();
+      System.out.println(Base64.getEncoder().encodeToString(encryptedAESKey));
+      System.out.println();
+      System.out.println(Base64.getEncoder().encodeToString(cipherText));
+
+
       
     } catch (Exception e) {
       System.out.println("ERROR: " + e.getMessage());
     }
     
+  }
+
+  // public static byte[] readBytes(String filePath, int start, int size) throws IOException {
+  //     File binaryFile = new File(filePath);
+  //     RandomAccessFile randomAccessFile = new RandomAccessFile(binaryFile, "r");
+  //     FileChannel binaryFileChannel = randomAccessFile.getChannel();
+
+  //     randomAccessFile.close();
+  //     return binaryFileChannel.map(FileChannel.MapMode.READ_ONLY, start, size).array();
+  // }
+
+  public static byte[] read(String file) throws IOException {
+    Path path = Paths.get(file);
+    byte[] data = Files.readAllBytes(path);
+    return data;
   }
 
   
